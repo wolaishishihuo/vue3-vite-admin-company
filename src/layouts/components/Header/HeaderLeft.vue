ca4319faf73fc90fa3ab2232c73107ad75f5b74f
jiazhigang <jiazhigang_888@163.com>
2025-06-20 17:22:07 +0800

    feat: 添加多种尺寸和主题的logo SVG图标

--- src/layouts/components/Header/HeaderLeft.vue
+++ src/layouts/components/Header/HeaderLeft.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="logo" :style="{ width: menuCollapse ? `${defaultMenuCollapseWidth}px` : `${defaultMenuWidth}px` }">
-    <div class="h-50 w-full" :class="menuCollapse ? 'i-svg:app' : 'i-svg:logo'" />
+    <div class="h-50 w-full" :class="menuCollapse ? isDark ? 'i-svg:logo-small-dark' : 'i-svg:logo-small-light' : isDark ? 'i-svg:logo-large-dark' : 'i-svg:logo-large-light'" />
   </div>
   <div class="cursor-pointer">
     <el-icon @click="visibleMenu">
@@ -10,12 +10,15 @@
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
 import appConfig from '@/config';
 import { useSettingStore } from '@/store/modules/setting';
 
 const { defaultMenuCollapseWidth, defaultMenuWidth } = appConfig.systemSetting;
 
 const settingStore = useSettingStore();
+const { isDark } = storeToRefs(settingStore);
+
 const menuCollapse = computed(() => settingStore.menuOpen);
 const visibleMenu = () => {
   settingStore.setMenuOpen(!settingStore.menuOpen);
