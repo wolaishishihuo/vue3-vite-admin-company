b0170c6e861d817d04d2d32d1f0b2089d23c1cde
jiazhigang <jiazhigang_888@163.com>
2025-06-21 10:53:15 +0800

    feat: 添加过渡动画样式，更新全局样式导入

--- src/plugins/setupAssets.ts
+++ src/plugins/setupAssets.ts
@@ -1,7 +1,8 @@
-import '@/styles/reset.scss'; // 全局样式
 import 'element-plus/theme-chalk/dark/css-vars.css';
 import 'virtual:uno.css';
+import '@/styles/reset.scss'; // 全局样式
 import '@/styles/theme-animation.scss';
 import '@/styles/el-ui.scss';
+import '@/styles/transition.scss';
 
 export const setupAssets = () => ({});
